f329018a5d106baaef8b4147aea6eaa2c7fdb3df
redesocialessmp <redesocialessmp@gmail.com>
2025-02-04 14:28:44 +0100

    Actualizacion correos y pdf
    Actualizacion añadiendo funciones de correo, creacion de pdf. Usando axigen, y librerias

--- Proyecto/funciones_admin/funcion_db.php
+++ Proyecto/funciones_admin/funcion_db.php
@@ -1,4 +1,9 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 function actualizarEstado()
 {
     include 'db.php';
@@ -164,9 +169,10 @@ function realizarBaremacion()
 function realizarCalculoBarem($codigo_curso)
 {
     include 'db.php';
-    $query = "SELECT solicitantes.dni, solicitantes.nombre, solicitantes.apellidos, solicitantes.puntos, solicitudes.codigocurso
+    $query = "SELECT solicitantes.dni, solicitantes.nombre, solicitantes.apellidos, solicitantes.puntos, solicitudes.codigocurso, cursos.nombre AS nombre_curso, solicitantes.correo
     FROM solicitantes
     INNER JOIN solicitudes ON solicitantes.dni = solicitudes.dni
+    INNER JOIN cursos ON solicitudes.codigocurso = cursos.codigo
     WHERE solicitudes.codigocurso = :codigocurso
     ORDER BY solicitantes.puntos DESC
     LIMIT :numeroPlazas";
@@ -184,7 +190,7 @@ function realizarCalculoBarem($codigo_curso)
     echo "<h3>Admitidos para el curso con código <strong>" . htmlspecialchars($codigo_curso) . "</strong>:</h3>";
     if ($stmt->rowCount() > 0) {
         while ($solicitante = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            actualizarAdmitidos($codigo_curso, $solicitante['dni'], $solicitante['nombre'], $solicitante['apellidos']);
+            actualizarAdmitidos($codigo_curso, $solicitante['dni'], $solicitante['nombre'], $solicitante['apellidos'], $solicitante['nombre_curso'], $solicitante['correo']);
         }
     } else {
         echo "<p>No hay solicitantes para el curso con código <strong>" . htmlspecialchars($codigo_curso) . "</strong>.</p>";
@@ -192,7 +198,6 @@ function realizarCalculoBarem($codigo_curso)
     echo "</div>";
     generarMenu();
 }
-
 
 function verNumeroPlazas($codigo_curso){
     include 'db.php';
@@ -205,7 +210,7 @@ function verNumeroPlazas($codigo_curso){
 }
 
 
-function actualizarAdmitidos($codigo_curso, $dni, $nombre, $apellidos){
+function actualizarAdmitidos($codigo_curso, $dni, $nombre, $apellidos, $nombre_curso, $email){
     include 'db.php';
     $query = "UPDATE solicitudes SET admitido = 1 WHERE codigocurso = :codigo_curso AND dni = :dni";
     $stmt = $conexion->prepare($query);
@@ -214,8 +219,91 @@ function actualizarAdmitidos($codigo_curso, $dni, $nombre, $apellidos){
     $stmt->execute();
     if ($stmt->rowCount() > 0) {
         echo "<p>Solicitante con nombre y apellidos <strong>" . htmlspecialchars($nombre) ." ".$apellidos. "</strong> admitido correctamente para el curso con código <strong>" . htmlspecialchars($codigo_curso) . "</strong>.</p>";
+        crearPDF($codigo_curso, $dni, $nombre, $apellidos, $nombre_curso, $email);
     }else{
         echo "<p>No se pudo admitir al solicitante con nombre y apellidos <strong>" . htmlspecialchars($nombre) ." ".$apellidos. "</strong> para el curso con código <strong>" . htmlspecialchars($codigo_curso) . "</strong>.</p>";
         echo "<p class='error'>Por favor, revisa que el solicitante no haya sido admitido previamente.</p>";
+    }
+}
+
+function crearPDF($codigo_curso, $dni, $nombre, $apellidos, $nombre_curso, $email){
+    // Incluir la librería FPDF
+    require_once('librerias/fpdf186/fpdf.php');
+
+    // Crear un objeto FPDF
+    $pdf = new FPDF();
+    $pdf->AddPage();
+    
+    //Agregar la imagen
+    $pdf->Image('img/logoIES.png', 10, 10, 50); //Para los tamaños de las mismas
+
+    // Establecer la fuente
+    $pdf->SetFont('Arial', 'B', 16);
+    $pdf->Cell(0, 10, 'Certificado de Admision', 0, 1, 'C');
+
+    // Información del admitido
+    $pdf->SetFont('Arial', '', 12);
+    $pdf->Ln(10);
+    $pdf->MultiCell(0, 10, "Estimado/a $nombre $apellidos,");
+    $pdf->Ln(5);
+    $pdf->MultiCell(0, 10, "Nos complace informarle que ha sido admitido/a en el curso '$nombre_curso'.");
+    $pdf->Ln(5);
+    $pdf->MultiCell(0, 10, "DNI: $dni");
+    $pdf->MultiCell(0, 10, "Codigo del curso: $codigo_curso");
+
+    // Mensaje de finalización
+    $pdf->Ln(10);
+    $pdf->MultiCell(0, 10, "Le deseamos mucho exito en su formación. Para mas informacion, contacte con la administración.");
+
+    // Creo la fecha para incorporarla en el pdf y darle nombre unico al pdf
+    $nuevaFecha = new DateTime(); 
+    echo $nuevaFecha->format("Y-m-d H:i:s") . "<br>";
+    $pdf->MultiCell(0, 10, "Fecha de admicion: " . $nuevaFecha->format("Y-m-d"));
+    
+    // Salvar o mostrar el PDF
+    $ruta_PDF = 'PDF/admitido_' . $dni . $codigo_curso . $nuevaFecha->format("Y-m-d H:i:s") . '.pdf';
+    $pdf->Output('F', $ruta_PDF);
+    $asunto = 'Admision en el curso '.$nombre_curso;
+    $mensaje = "Estimado/a $nombre $apellidos,\n\nNos complace informarle que ha sido admitido/a en el curso '$nombre_curso'.\n\nDNI: $dni\nCodigo del curso: $codigo_curso\n\nLe deseamos mucho exito en su formacion. Para mas informacion, contacte con la administracion.";
+    enviarCorreo($email, $asunto, $mensaje, $ruta_PDF);
+}
+
+function enviarCorreo($email, $asunto, $mensaje, $ruta){
+    spl_autoload_register(function ($clase) {
+        $fullpath = "librerias/PHPMailer-master/src/" . $clase . ".php";
+        if (file_exists($fullpath)) {
+            require_once $fullpath;
+        } else {
+            echo "<p>La clase $fullpath no se encuentra</p>";
+            exit();
+        }
+    
+    });
+    $mail = new PHPMailer(true);
+    $mail->SMTPDebug = 0; // Habilita la salida de depuración detallada
+    $mail->isSMTP();                       // Establece el tipo de correo electrónico para usar SMTP
+    $mail->Host     = 'localhost';         // Especifica los servidores SMTP principales y de respaldo
+    $mail->SMTPAuth = false;
+    $mail->Username = 'admin';
+    $mail->Password = 'qm#.FMb*7uy';
+    // $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;   // Habilita el cifrado TLS; `ssl` también aceptado
+    $mail->Port = 587;
+    try {
+        // Configura y envía el mensaje
+        $mail->setFrom('admin@cursos.com', 'admin');
+        $mail->addAddress($email);
+        $mail->Subject = $asunto;
+        $mail->Body    = $mensaje;
+
+        // Adjuntar pdf si la ruta es valida
+        if (!empty($ruta) && file_exists($ruta)) {
+            $mail->addAttachment($ruta); // Adjunta la imagen
+        } else {
+            $mail->Body .= "\n\nNo se encontra el archivo adjunto en la ruta especificada.";
+        }
+        $mail->send();
+    } catch (Exception $e) {
+        echo 'El mensaje no pudo ser enviado.';
+        echo 'Error de correo: ' . $mail->ErrorInfo;
     }
 }
